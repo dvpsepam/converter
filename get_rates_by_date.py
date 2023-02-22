17a125387ff5feafbf22e9c1d290685b62d07d5b
Andrii Rusin <aws.motolok@gmail.com>
2023-02-22 13:18:36 +0200

    rename py file

--- get_rates_by_date.py
+++ get_rates_by_date.py
@@ -2,7 +2,9 @@ import os
 import datetime# import datetime
 import json
 from urllib.request  import urlopen
+import pandas as pd 
 
+url_kursy = "http://api.nbp.pl/api/exchangerates/rates/a/usd/2022-01-01/2022-12-31/"
 
 # specify the year for which you want to get the weekdays
 year = 2022
@@ -10,7 +12,7 @@ year = 2022
 # create a datetime object for the first day of the year
 date = datetime.date(year, 1, 1)
 
-def get_dates_from_api():
+def get_workdays_dates():
     # loop over all the days in the year
     while date.year == year:
         # check if the day is a weekday (Monday = 0, Sunday = 6)
@@ -21,12 +23,12 @@ def get_dates_from_api():
         # increment the date by one day
         date += datetime.timedelta(days=1)
 
-url_kursy = "http://api.nbp.pl/api/exchangerates/rates/a/usd/2022-01-01/2022-12-31/"
+# read 
+def read_from_api():
+        
+    data_from_bank = urlopen(url_kursy)
+    dict_kursy = json.loads(data_from_bank.read())
 
-data_from_bank = urlopen(url_kursy)
-dict_kursy = json.loads(data_from_bank.read())
-#print(dict_kursy)
+    for rate in dict_kursy["rates"]:
+        print(datetime.datetime.strptime(rate["effectiveDate"], '%Y-%m-%d'))
 
-for rate in dict_kursy["rates"]:
-    print(datetime.datetime.strptime(rate["effectiveDate"], '%Y-%m-%d'))
-#print(type(date(rate["effectiveDate"])), rate["mid"])
